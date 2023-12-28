f2c505c59cad3233ce7f5736b29c3804fee8356a
droths <david.rothschild@uzh.ch>
2023-12-28 09:40:35 +0100

    change in Fig.

--- R/hs_ol.R
+++ R/hs_ol.R
@@ -1,19 +1,29 @@
-# Import a pickle file
+
 library(ggplot2)
-ol <- ggplot(data = labeled_data, aes(x=offensive_language, y= count)) + geom_col()+theme_minimal()
-
-
-hs <- ggplot(data = labeled_data, aes(x=hate_speech, y= count)) + geom_col()+theme_minimal()
-
-# combine the plots ol and hs
 library(cowplot)
-plot_grid(ol, hs, labels = c( "offensive_language", "hate_speech"), ncol = 1)))
 
-# plot the combined plot
-plot_grid(ol, hs, labels = c( "offensive_language", "hate_speech"), ncol = 1)
+# Define a color palette
+my_colors <- c("blue", "red")
 
+# Plot for offensive_language
+ol <- ggplot(data = labeled_data, aes(x=offensive_language, y=count)) +
+  geom_col(fill=my_colors[1]) +
+  theme_minimal() +
+  labs(title="Offensive Language Analysis",
+       x="Offensive Language",
+       y="Count")
 
-# plot the combined plot
-plot_grid(ol, hs, labels = c( "offensive_language", "hate_speech"), ncol = 1)
+# Plot for hate_speech
+hs <- ggplot(data = labeled_data, aes(x=hate_speech, y=count)) +
+  geom_col(fill=my_colors[2]) +
+  theme_minimal() +
+  labs(title="Hate Speech Analysis",
+       x="Hate Speech",
+       y="Count")
 
+# Combine the plots
+combined_plot <- plot_grid(ol, hs, ncol = 1, align = 'v', axis = 'tblr',
+                           label_size = 15, label_fontface = 'bold')
 
+# Print the combined plot
+print(combined_plot)
